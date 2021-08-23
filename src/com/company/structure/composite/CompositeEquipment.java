06264330baa827162afc43109fb715b3d6a3c93e
jooomOut <rimeilo324@naver.com>
2021-08-23 17:40:05 +0900

    feat(composite) : add CompositeEquipment, implement getNetPrice
    get sum of netPrice of all Equipment in CompositeEquipment

--- src/com/company/structure/composite/CompositeEquipment.java
+++ src/com/company/structure/composite/CompositeEquipment.java
@@ -4,22 +4,28 @@ import java.util.Currency;
 import java.util.Iterator;
 import java.util.List;
 
-public class FloppyDisk implements Equipment {
+public class CompositeEquipment implements Equipment {
+    private List<Equipment> equipmentList;
+
     @Override
     public String getName() {
         return null;
     }
-
-    @Override
+    // creatae iterator를 사용해 구성요소를 구하고 각 장비마다 netprice 호출해 총합 리턴
+   @Override
     public int getNetPrice() {
-        return null;
+        int ret = 0;
+        for(Equipment equipment : this.equipmentList) {
+            ret += equipment.getNetPrice();
+        }
+        return ret;
     }
 
     @Override
     public Currency getDiscountPrice() {
         return null;
     }
-
+    // 장비 리스트에 장비를 추가.
     @Override
     public void add(Equipment equipment) {
 
@@ -29,9 +35,9 @@ public class FloppyDisk implements Equipment {
     public void remove(Equipment equipment) {
 
     }
-
+    // 장비 리스트 순
     @Override
     public List<Equipment> createIterator() {
-        return null;
+        return this.equipmentList;
     }
 }
--- src/com/company/structure/composite/Equipment.java
+++ src/com/company/structure/composite/Equipment.java
@@ -3,15 +3,16 @@ package com.company.structure.composite;
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.Iterator;
+import java.util.List;
 
 public interface Equipment<name> {
     public String getName();
     //public Watt getPower();
-    public Currency getNetPrice();
+    public int getNetPrice();
     public Currency getDiscountPrice();
 
     public void add(Equipment equipment);
     public void remove(Equipment equipment);
-    public Iterator<Equipment> createIterator();
+    public List<Equipment> createIterator();
 
 }
