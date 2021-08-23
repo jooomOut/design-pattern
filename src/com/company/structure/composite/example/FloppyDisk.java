f929304cfb9cc1d5b0a901a986f82ab4869728ee
jooomOut <rimeilo324@naver.com>
2021-08-23 20:05:46 +0900

    feat(composite/example) : remove useless method

--- src/com/company/structure/composite/example/FloppyDisk.java
+++ src/com/company/structure/composite/example/FloppyDisk.java
@@ -4,32 +4,15 @@ import java.util.Currency;
 import java.util.List;
 
 public class FloppyDisk extends Equipment {
+    private int netPrice;
+
     FloppyDisk(String name){
         this.name = name;
     }
 
     @Override
     public int getNetPrice() {
-        return 0;
+        return this.netPrice;
     }
 
-    @Override
-    public Currency getDiscountPrice() {
-        return null;
-    }
-
-    @Override
-    public void add(Equipment equipment) {
-
-    }
-
-    @Override
-    public void remove(Equipment equipment) {
-
-    }
-
-    @Override
-    public List<Equipment> createIterator() {
-        return null;
-    }
 }
